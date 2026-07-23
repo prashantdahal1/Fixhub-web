f7452c29c51b79ec4db0541d4b3f38cb1988a274
prashantdahal1 <prashantdahal319@gmail.com>
2026-07-23 10:00:00 +0545

    chore: setup backend entry point and dependencies

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,19 @@
-import app, { PORT, DUMMY } from "./src/app";
-// importing same variable 
-import { PORT as API_PORT } from "./src/configs/constant";
+import app from "./src/app";
+import { createServer } from "http";
 import { connectToMongoDB } from "./src/database/mongodb";
+import { PORT as API_PORT } from "./src/configs/constant";
+import { createRealtimeWebSocketServer } from "./src/utils/realtime.util.js";
 
-connectToMongoDB();
+const startServer = async () => {
+  await connectToMongoDB();
+  const server = createServer(app);
+  createRealtimeWebSocketServer(server);
 
-app.listen(API_PORT, '0.0.0.0', () => {
+  server.listen(API_PORT, '0.0.0.0', () => {
     console.log(`Server: http://localhost:${API_PORT}`);
-});
+  });
+};
+
+startServer();
 // execute: npx tsx --watch index.ts
 // http://localhost:8089
